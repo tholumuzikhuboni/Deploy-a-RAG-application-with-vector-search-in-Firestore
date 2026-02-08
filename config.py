f38be0289c2aabce56ee022ffd8b62ef82b1ad1b
Tholumuzi Kuboni <tholumuzi@linkedme.co.za>
2026-02-08 11:57:20 -0800

    Create config.py with lab configuration
    Add configuration settings for the lab project.

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# LAB CONFIGURATION
+PROJECT_ID = "qwiklabs-gcp-02-abd870b86810"  # <--- Change this to your Lab Project ID
+REGION = "us-central1"
+
+# APP CONSTANTS
+COLLECTION_NAME = "food-safety"
+EMBEDDING_MODEL_NAME = "text-embedding-005"
+GENERATIVE_MODEL_NAME = "gemini-2.0-flash"
